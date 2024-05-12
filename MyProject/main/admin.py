1c3e40b9c33f81d9df88288eea601ab8a1fe306b
Георги Бориков <pgee_dzi2024@abv.bg>
2024-05-12 17:21:45 +0300

    Презентация, чернова

--- MyProject/main/admin.py
+++ MyProject/main/admin.py
@@ -5,6 +5,7 @@ admin.site.register(Employee)
 admin.site.register(BookHour)
 admin.site.register(Service)
 
+
 @admin.register(Appointment)
 class ImportantV(admin.ModelAdmin):
     list_display = ('id', 'ap_date', 'ap_hour', 'ap_service', 'ap_name', 'ap_phone', 'ap_new')
